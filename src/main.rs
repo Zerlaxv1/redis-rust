be31583a3c427619235834e3282f6601b76f9c7e
Nino DUCCESCHI <ducceschi@ga.fr>
2026-04-17 11:21:40 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,43 +13,45 @@ async fn main() {
     // main loop
     loop {
         // accepte new incoming connections
-        let (stream, _): (TcpStream,_) = listener.accept().await.unwrap();
+        let (stream, _): (TcpStream, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
             handle_connection(stream).await;
         });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> bool {
+async fn handle_connection(mut stream: TcpStream) {
     let buffer_response = b"+PONG\r\n";
     let mut buffer_input = [0; 512];
 
-    // write input to buffer
-    let input: Result<usize, std::io::Error> = stream.read(&mut buffer_input).await;
+    loop {
+        // write input to buffer
+        let input: Result<usize, std::io::Error> = stream.read(&mut buffer_input).await;
 
-    // if not input, ignore
-    match input {
-        Ok(bytes) => {
-            if bytes == 0 {
-                return false;
-            }
-
-            // envoyer la réponse
-            let result: Result<(), std::io::Error> = stream.write_all(buffer_response).await;
-
-            match result {
-                Ok(_) => {
-                    return true;
+        // if not input, ignore
+        match input {
+            Ok(bytes) => {
+                if bytes == 0 {
+                    return;
                 }
-                Err(_) => {
-                    eprintln!("Erreur lors de l'envoie de la réponse");
-                    return false;
+
+                // envoyer la réponse
+                let result: Result<(), std::io::Error> = stream.write_all(buffer_response).await;
+
+                match result {
+                    Ok(_) => {
+                        return;
+                    }
+                    Err(_) => {
+                        eprintln!("Erreur lors de l'envoie de la réponse");
+                        return;
+                    }
                 }
             }
-        }
-        Err(_) => {
-            eprintln!("Erreur lors de la lecture de l'input");
-            return false;
+            Err(_) => {
+                eprintln!("Erreur lors de la lecture de l'input");
+                return;
+            }
         }
     }
 }
